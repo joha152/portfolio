fa0efe65ad1f07ae019e825171ebfaf77b83079c
joha152 <johanna.servodio@hotmail.com.ar>
2023-06-22 17:29:52 -0300

    sobre mi

--- src/componentes/sobreMi/SobreMi.jsx
+++ src/componentes/sobreMi/SobreMi.jsx
@@ -5,7 +5,14 @@ const SobreMi = ()=> {
 
     return(
         <>
-      <h2>no tengo ni idea de quien soy </h2>
+      <h1>Hola</h1>
+      <h2>Soy Johanna, soy de Argentina</h2>
+      <h3>Desarrolle mis conocimientos en Henry</h3>
+      <h3>Conoci Henry gracias a una amiga. Al principio no sabia donde estaba ni lo que estaba haciendo,
+         pero cuando entendí todas las posibilidades que hay al programar, me encantó!! 
+      </h3>
+      <h3>Fue un camino dificil pero no me arrepiento de todo el sacrificio que fue llegar a la meta</h3>
+      
         </>
     )
 
