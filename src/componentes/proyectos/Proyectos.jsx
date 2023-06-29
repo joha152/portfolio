76573ba822057775ec6c05d4480d4cae487876e8
joha152 <johanna.servodio@hotmail.com.ar>
2023-06-29 18:05:53 -0300

    responsive

--- src/componentes/proyectos/Proyectos.jsx
+++ src/componentes/proyectos/Proyectos.jsx
@@ -25,14 +25,24 @@ const Proyectos = () => {
                     <a href="https://deploy-food-eight.vercel.app/">
                         <h2 className={style.texto}>Food App</h2>
                     </a>
-
+                        <p className={style.parrafo}>Este proyecto fue realizado con Javascript, React.js, Redux, Node.js, Express y Sequelize.
+                            Realiza busqueda por nombre de receta entera o coincidencia de una palabra. Filtra por tipo de dieta,
+                            y por origen, si es de la api o de la base de datos, ya que permite crear recetas y guardarlas.
+                            Ordena por heslthScore, y alfabeticamente.
+                        </p>
                     <img src={food} alt="food" className={style.img} />
                 </div>
                 <div className={style.card}>
                     <a href="https://muscle-lab-six.vercel.app/">
                         <h2 className={style.texto}>MuscleLab</h2>
                     </a>
-
+                        <p className={style.parrafo}>
+                            MuscleLab fue creado con la intención de poder administrar la página de un gimnasio.
+                            Por parte del administrador se pueden crear, editar y eliminar clases, usuarios y sedes.
+                            Por parte del cliente se puede crear un usuario, o acceder con Google. Comprar una membresía 
+                            que permite una cierta cantidad de clases, que se seleccionan y se guardan en el perfil. También pueden 
+                            hacer comentarios de las clases tomadas.
+                        </p>
                     <img src={muscleLab} alt="muscleLab" className={style.img} />
                 </div>
             </div>
